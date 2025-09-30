ce6f20428c5d95f6586411af1d7b7f51c5a7dce4
LuisfNino <gproductdevelopment@gmail.com>
2025-09-30 13:13:52 +0000

    created ch00_copy

---
+++ src/ch00_copy/CodeDemo.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+int main(){
+    float num_1, num_2, result;
+
+    std::cout << "Enter number 1:" << std::flush;
+    std::cin >> num_1;
+    std::cout << "Enter number 2:" << std::flush;
+    std::cin >> num_2;
+
+    result = num_1 + num_2;
+
+    
+
+}

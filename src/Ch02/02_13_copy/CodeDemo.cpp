b5192e2382462288f85fca69452469ce6a00ebd3
LuisfNino <gproductdevelopment@gmail.com>
2025-09-30 20:36:28 +0000

    completed 02_13_copy

---
+++ src/Ch02/02_13_copy/CodeDemo.cpp
@@ -0,0 +1,25 @@
+// Learning C++ 
+// Challenge 02_13
+// Calculate an Average, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main(){
+    int nums[5] = {1,23,32,24,337};
+    float result; 
+
+    // Write your code here
+    result += nums[0];
+    result += nums[1];
+    result += nums[2];
+    result += nums[3];
+    result += nums[4];
+    result /= sizeof(nums)/sizeof(nums[0]);
+
+    // NOTE: sizeof(nums) returns the bytes, not the element count
+
+    std::cout << "The average is " << result << std::endl;
+    
+    std::cout << std::endl << std::endl;
+    return (0);
+}

9054b62d78f9872c27dd9776d0eb99d9920b5925
LuisfNino <gproductdevelopment@gmail.com>
2025-09-30 14:33:08 +0000

    ch01_02_b completed

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,13 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream> // to communicate with the terminal
+
+int main(){
+    // Block of code
+    std::cout << "Hola que tal" << std::endl;
+
+    std::cout << std::endl << std::endl;
+    return (0);
+}

dc2f1b582eebe12294febf8e1e84c5bdf94cccf6
LuisfNino <gproductdevelopment@gmail.com>
2025-10-01 07:54:56 +0000

    completed 03_07b

--- src/Ch03/03_07b/CodeDemo.cpp
+++ src/Ch03/03_07b/CodeDemo.cpp
@@ -2,14 +2,21 @@
 // Exercise 03_07
 // Using Objects with Pointers, by Eduardo Corpeño 
 
+// pointers to dynamically allocate memory for new objects when calling the class constructor and freeing the memory when the objects are no longer needed
+
 #include <iostream>
 #include <string>
 #include "cow.h"
 
 int main(){
-    cow my_cow("Hildy", 7, cow_purpose::pet);
-    std::cout << my_cow.get_name() << " is a type-" << (int) my_cow.get_purpose() << " cow." << std::endl;
-    std::cout << my_cow.get_name() << " is " << my_cow.get_age() << " years old." << std::endl;
+    // object dynamically created
+    cow *my_cow; 
+    my_cow = new cow("Hildy", 7, cow_purpose::pet);
+    std::cout << my_cow->get_name() << " is a type-" << (int) my_cow->get_purpose() << " cow." << std::endl;
+    std::cout << my_cow->get_name() << " is " << my_cow->get_age() << " years old." << std::endl;
+
+    // delete object to when no longer needed to avoid memory leakage
+    delete my_cow; 
     
     std::cout << std::endl << std::endl;
     return (0);

924b13df3a119438a8bb79bcf49239161872a7d5
LuisfNino <gproductdevelopment@gmail.com>
2025-10-01 08:03:31 +0000

    completed 03_09b

--- src/Ch03/03_09b/CodeDemo.cpp
+++ src/Ch03/03_09b/CodeDemo.cpp
@@ -2,11 +2,25 @@
 // Exercise 03_09
 // Vectors, by Eduardo Corpeño 
 
+// Vectors are dynamic-size containers
+// Memory is managed for you
+
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main(){
-    
+    std::vector<int> primes;
+    primes.push_back(2);
+    primes.push_back(3);
+
+    std::cout << "vectors size is: " << primes.size() << std::endl;
+
+    primes[0] = 13;
+
+    std::cout << primes[0] << std::endl;
+    std::cout << primes[1] << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }

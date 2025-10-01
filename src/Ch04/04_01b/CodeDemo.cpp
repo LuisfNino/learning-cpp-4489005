ed4e65db7fe78262c1f3135b276db7e4b91571d3
LuisfNino <gproductdevelopment@gmail.com>
2025-10-01 08:47:13 +0000

    completed 04_01b

--- src/Ch04/04_01b/CodeDemo.cpp
+++ src/Ch04/04_01b/CodeDemo.cpp
@@ -5,7 +5,26 @@
 #include <iostream>
 
 int main(){
-    
+    int a = 1023;
+    bool flag = false;
+    char lttr = 'd';
+
+    if (a > 1000)
+        std::cout << "Warning: " << std::endl;
+
+    if (a % 2)
+        std::cout << "Odd number" << std::endl;
+    else
+        std::cout << "Even number" << std::endl;
+
+    if (lttr != 'a' && lttr != 'e' && lttr != 'i' && lttr != 'o' && lttr != 'u')
+        std::cout << "Not vowel" << std::endl;
+
+    if (flag)
+        std::cout << "is true" << std::endl;
+    else
+        std::cout << "is flase" << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }

24bbcfd5f9a52bf62f9dcad19b8aa61e4bea0cee
LuisfNino <gproductdevelopment@gmail.com>
2025-10-01 13:02:08 +0000

    completed 05_02b

--- src/Ch05/05_02b/CodeDemo.cpp
+++ src/Ch05/05_02b/CodeDemo.cpp
@@ -4,13 +4,36 @@
 
 #include <iostream>
 
+// passing arguments by value
+int square (int x) {
+    x = x*x;
+    return x;
+}
+
+// passing arguments by pointer
+void swap(int *x, int *y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+// passing arguments by reference
+void swap (int& x, int& y){
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
 int main(){
     int a = 9, b;
     // TODO: square
+    b = square (a);
     std::cout << "a = " << a << ", b = " << b << std::endl;
     // TODO: swap
+    swap (&a, &b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
     // TODO: swap
+    swap(a,b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
     
     std::cout << std::endl << std::endl;

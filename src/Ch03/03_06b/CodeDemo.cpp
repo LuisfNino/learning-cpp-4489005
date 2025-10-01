04ede1c191cf7c84126f95b3bd87e30f157ee798
LuisfNino <gproductdevelopment@gmail.com>
2025-10-01 07:41:37 +0000

    completed 03_06b

--- src/Ch03/03_06b/CodeDemo.cpp
+++ src/Ch03/03_06b/CodeDemo.cpp
@@ -6,6 +6,16 @@
 #include <string>
 
 int main(){
+    int a = 37;
+    int *ptr;
+
+    ptr = &a;
+
+    std::cout << "content of a: " << a << std::endl;
+    std::cout << "address of a: " << &a << std::endl << std::endl;
+    std::cout << "content of ptr: " << ptr << std::endl;
+    std::cout << "address of ptr: " << &ptr << std::endl;
+    std::cout << "content of where ptr is pointing: " << *ptr << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);

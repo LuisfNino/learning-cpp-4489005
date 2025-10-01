47a0f2bd897e558083554b1c1696e697112441fe
LuisfNino <gproductdevelopment@gmail.com>
2025-10-01 08:58:21 +0000

    completed 04_03b

--- src/Ch04/04_03b/CodeDemo.cpp
+++ src/Ch04/04_03b/CodeDemo.cpp
@@ -9,7 +9,22 @@ int main(){
     std::vector<int> numbers = {12, 25, 31, 47, 58};
     
     std::vector<int>::iterator ptr = numbers.begin();
-    
+
+    // move with a pointer along the vector
+    while (ptr != numbers.end()){
+        std::cout << *ptr << " ";
+        // move the vector position forward
+        ptr = next(ptr,1); 
+    }
+    std::cout << std::endl << std::endl;
+
+    // move with an indicator along the vector
+    int i = 0;
+    do{
+        std::cout << numbers[i] << " ";
+        i++;
+    } while (i < numbers.size());
+
     std::cout << std::endl << std::endl;
     return (0);
 }
